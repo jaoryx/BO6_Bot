2757ac82a0b48f572f1514ef51adef7322f24bb0
JaoryxHowest <jordan.baveghems@student.howest.be>
2025-02-12 14:16:34 +0100

    Add functionality to unlock camo and camo's below

--- events/manageCamoInteraction.js
+++ events/manageCamoInteraction.js
@@ -5,6 +5,23 @@ const modes = {
     "Zombies": "zm"
 }
 
+const camoIndexes = {
+    1: [0, 1],
+    2: [0, 1, 2],
+    3: [0, 1, 2, 3],
+    4: [0, 1, 2, 3, 4],
+    5: [0, 1, 2, 3, 4, 5],
+    6: [0, 1, 2, 3, 4, 5, 6],
+    7: [0, 1, 2, 3, 4, 5, 6, 7],
+    8: [0, 1, 2, 3, 4, 5, 6, 7, 8], // Unlocks all Military camo's and below
+    9: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], // Unlocks Special camo + all Military's
+    10: [0, 1, 2, 3, 4, 5, 6, 7, 8, 10], // Unlocks Special camo + all Military's
+    11: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], // Unlocks Gold + all below
+    12: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], // Unlocks Diamond + all below
+    13: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], // Unlocks Dark Spine + all below
+    14: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], // Unlocks Dark Matter + all below
+}
+
 module.exports = {
     name: Events.InteractionCreate,
     once: false,
@@ -22,11 +39,18 @@ module.exports = {
         let userData = await client.GetUser(interaction.user.id);
         let weapon = userData[modes[userSelectReplies.mode]].find(el => el.weaponName == userSelectReplies.weapon);
         let camo = weapon.camos.find(el => el.camoName == userSelectReplies.camo);
+        let camoIndex = weapon.camos.findIndex(el => el.camoName == userSelectReplies.camo);
+        
+        let camoIndexesToUnlock = camoIndexes[camoIndex];
+
         let msg = ``;
 
         if (interaction.customId === 'addcamo') {
-            camo.obtained = true;
-            msg = `The **${userSelectReplies.camo}** camo for the ${weapon.weaponType} **${userSelectReplies.weapon}** has been added to your profile!`;
+            camoIndexesToUnlock.forEach(index => {
+                weapon.camos[index].obtained = true;
+            });
+            //camo.obtained = true;
+            msg = `The **${userSelectReplies.camo}** camo and the camo's below for the ${weapon.weaponType} **${userSelectReplies.weapon}** has been added to your profile!`;
         } else if (interaction.customId === 'removecamo') {
             camo.obtained = false;
             msg = `The **${userSelectReplies.camo}** camo for the ${weapon.weaponType} **${userSelectReplies.weapon}** has been removed from your profile!`;
