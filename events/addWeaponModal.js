1006d64b9864edb4ce70c17aa3b138c82a82e335
JaoryxHowest <jordan.baveghems@student.howest.be>
2025-02-21 12:00:34 +0100

    Update add weapon module

--- events/addWeaponModal.js
+++ events/addWeaponModal.js
@@ -1,4 +1,40 @@
-const { Events, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { Events, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
+
+const defaultMpCamos = [
+    'Granite',
+    'Woodland',
+    'Savanna',
+    'Splinter',
+    'Moss',
+    'Saboteur',
+    'Digital',
+    'Tide',
+    'Red Tiger',
+    'Special',
+    'Special',
+    'Gold',
+    'Diamond',
+    'Dark Spine',
+    'Dark Matter'
+];
+
+const defaultZmCamos = [
+    'Slate',
+    'Desert',
+    'Evergreen',
+    'Rugged',
+    'Grim',
+    'Stripe',
+    'Oceanic',
+    'Whiteout',
+    'Purple Tiger',
+    'Special',
+    'Special',
+    'Mystic Gold',
+    'Opal',
+    'Afterlife',
+    'Nebula'
+];
  
 module.exports = {
     name: Events.InteractionCreate,
@@ -12,20 +48,28 @@ module.exports = {
 
             client.weaponDetails[interaction.user.id] = { name: weaponName, type: weaponType };
 
-            const addCamosButton = new ButtonBuilder()
-                .setCustomId('addCamosButton')
-                .setLabel('Add Special Camos')
+            const addMpCamosButton = new ButtonBuilder()
+                .setCustomId('addMpCamosButton')
+                .setLabel('Add MP Special Camos')
                 .setStyle(ButtonStyle.Primary);
 
-            const addCamosRow = new ActionRowBuilder().addComponents(addCamosButton);
+            const addZmCamosButton = new ButtonBuilder()
+                .setCustomId('addZmCamosButton')
+                .setLabel('Add ZM Special Camos')
+                .setStyle(ButtonStyle.Primary);
 
-            await interaction.reply({ content: `Currently adding ${weaponName}!`, components: [addCamosRow], ephemeral: true });
-        } else if(interaction.customId === 'addCamosButton') {
+            const addCamosRow = new ActionRowBuilder().addComponents(addMpCamosButton, addZmCamosButton);
+
+            await interaction.reply({ content: `Currently adding ${weaponName}!`, components: [addCamosRow] });
+            client.weaponDetails[interaction.user.id].msg = await interaction.fetchReply();
+        } else if (interaction.customId === 'addMpCamosButton' || interaction.customId === 'addZmCamosButton') {
             const weaponDetails = client.weaponDetails[interaction.user.id];
 
+            const isMp = interaction.customId === 'addMpCamosButton' ? true : false;
+
             const camoModal = new ModalBuilder()
-                .setCustomId('addCamosModal')
-                .setTitle(`Add the special camos for ${weaponDetails.name}`);
+                .setCustomId(isMp ? 'addMpCamosModal' : 'addZmCamosModal')
+                .setTitle(`Add the ${isMp ? 'Multiplayer' : 'Zombies'} special camos for ${weaponDetails.name}`);
 
             let textFields = [];
         
@@ -67,7 +111,59 @@ module.exports = {
 
             camoModal.addComponents(rows);
 
+            weaponDetails.buttonId = interaction.customId;
+
             await interaction.showModal(camoModal);
+        } else if (interaction.customId === 'addMpCamosModal' || interaction.customId === 'addZmCamosModal') {
+            await interaction.deferUpdate();
+            const isMp = interaction.customId === 'addMpCamosModal' ? true : false;
+
+            const camo1name = interaction.fields.getTextInputValue('camo1name');
+            const camo1desc = interaction.fields.getTextInputValue('camo1desc');
+            const camo2name = interaction.fields.getTextInputValue('camo2name');
+            const camo2desc = interaction.fields.getTextInputValue('camo2desc');
+
+            const specialCamos = [
+                { name: camo1name, desc: camo1desc, mode: isMp ? 'Multiplayer' : 'Zombies' },
+                { name: camo2name, desc: camo2desc, mode: isMp ? 'Multiplayer' : 'Zombies' },
+            ];
+
+            let weaponDetails = client.weaponDetails[interaction.user.id];
+
+            if (!weaponDetails.specialCamos) {
+                weaponDetails.specialCamos = specialCamos;
+                const oldRow = weaponDetails.msg.components[0];
+                const newRow = new ActionRowBuilder();
+    
+                oldRow.components.map((button) => { 
+                    const btn = ButtonBuilder.from(button);
+                    btn.setDisabled(button.customId === weaponDetails.buttonId);
+                    newRow.addComponents(btn);
+                    return btn;
+                });
+    
+                await weaponDetails.msg.edit({ content: `Currently adding ${weaponDetails.name}!`, components: [newRow] });
+            } else {
+                weaponDetails.specialCamos.push(...specialCamos);
+                let mpCamos = weaponDetails.specialCamos.filter(el => el.mode === 'Multiplayer');
+                let zmCamos = weaponDetails.specialCamos.filter(el => el.mode === 'Zombies');
+                let embed = new EmbedBuilder()
+                    .setTitle(`Overview of adding new weapon`)
+                    .setDescription(`Name: ${weaponDetails.name}\nType: ${weaponDetails.type}\nSpecial camo's:`)
+                    .addFields(
+                        { name: 'Multiplayer Camos', value: `Name: ${mpCamos[0].name}\nRequirement: ${mpCamos[0].desc}\n\nName: ${mpCamos[1].name}\nRequirement: ${mpCamos[1].desc}` },
+                        { name: 'Zombies Camos', value: `Name: ${zmCamos[0].name}\nRequirement: ${zmCamos[0].desc}\n\nName: ${zmCamos[1].name}\nRequirement: ${zmCamos[1].desc}` }
+                    );
+                
+                let btnRow = new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId('confirmWeapon').setLabel('Save Weapon').setStyle(ButtonStyle.Success));
+
+                weaponDetails.msg.edit({ embeds: [embed], components: [btnRow] });
+            }
+        } else if (interaction.customId === 'confirmWeapon') {
+            let weaponDetails = client.weaponDetails[interaction.user.id];
+
+            let mpCamos = weaponDetails.specialCamos.filter(el => el.mode === 'Multiplayer');
+            let zmCamos = weaponDetails.specialCamos.filter(el => el.mode === 'Zombies');
         }
     },
 };
